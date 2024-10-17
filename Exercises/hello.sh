a897fd3fdc51067ebf485c738c3711136aa5e5fb
0ttawa <skert@Ottawa.lan>
2024-10-17 21:23:19 +0100

    very simple bash menu

--- Exercises/hello.sh
+++ Exercises/hello.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 echo Chose one:
-echo
 echo 1.Goku
 echo 2.Vegeta
 
 read choice
 
-name=$chocie
-
-if [ $name=1 ]; then
-
-echo Hello, Goku, you little monkey !
-
-fi
-
-if [ $name=2 ]; then
-
-echo ITS OVER 9000!
-
-fi
+case $choice in
+            1)
+                echo "Hello Goku";;
+            
+            2)
+                echo "hello Vegeta";;
+            *)
+                echo wrong choice;;
+esac
