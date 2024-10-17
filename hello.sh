c254d760628c74c55fee96a89adfc17d0c11fa94
0ttawa <skert@Ottawa.lan>
2024-10-17 19:35:54 +0100

    first commit

---
+++ hello.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo Chose one:
+echo
+echo 1.Goku
+echo 2.Vegeta
+
+read choice
+
+currentUser=$choice
+
+if
+ $currentUser=1
+
+echo Hello, $currentUser, you little monkey !
+fi
+if
+$currentUser=2
+
+echo ITS OVER 9000!
+fi
