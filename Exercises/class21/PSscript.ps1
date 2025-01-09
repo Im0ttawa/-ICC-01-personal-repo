25bfacc4654b8278e2d6ea6f32ce276b4a5384fa
Alex <cs.dev.alex.barros@proton.me>
2025-01-09 17:05:34 +0000

    Add files via upload

---
+++ Exercises/class21/PSscript.ps1
@@ -0,0 +1,31 @@
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Path to the CSV file containing user information
+$users = Import-Csv -Path "Path\To\Your\File.csv"
+
+# Loop through each user in the CSV
+$users | ForEach-Object {
+    # Assign user attributes from the CSV
+    $FirstName = $_.FirstName
+    $LastName = $_.LastName
+    $UserName = $_.UserName
+    $Department = $_.Department
+    $Password = ConvertTo-SecureString $_.Password -AsPlainText -Force
+    $OU = "OU=$Department,DC=gruta,DC=local"
+
+    # Create the new AD user
+    New-ADUser `
+        -SamAccountName $UserName `
+        -UserPrincipalName "$UserName@gruta.com" `
+        -Name "$FirstName $LastName" `
+        -GivenName $FirstName `
+        -Surname $LastName `
+        -DisplayName "$FirstName $LastName" `
+        -Department $Department `
+        -AccountPassword $Password `
+        -ChangePasswordAtLogon $true `
+        -Enabled $true `
+        -Path $OU `
+        -PassThru
+}
